14f6620c2efb2286964cb72f0a03825574b82b79
yunhyukNam <DoubleY031223@gmail.com>
2023-03-15 23:03:45 +0900

    Chapter 8 Clear

--- 01.Kernel32/Source/Main.c
+++ 01.Kernel32/Source/Main.c
@@ -1,9 +1,33 @@
 #include "Types.h"
 
 void kPrintString( int iX, int iY, const char* pcString );
+BOOL kInitializeKernel64Area(void);
+BOOL kIsMemoryEnough(void);
 
 void main(void){
-    kPrintString( 0, 3, "C Language Kernel Started!!" );
+    DWORD i;
+
+    kPrintString( 0, 3, "C Language Kernel Start.....................[Pass]");
+
+    kPrintString( 0, 4, "Minimum Memory Size Check...................[    ]");
+    if(kIsMemoryEnough() == FALSE){
+        kPrintString( 45, 4, "Fail");
+        kPrintString( 0, 5, "Not Enough Memory! MINT64 OS Requires Over \"64Mbyte Memory!!\"");
+        
+        while(1);
+    }
+    else{
+        kPrintString( 45, 4, "Pass");
+    }
+
+    kPrintString(0, 5, "IA-32e Kernel Area Initialization...........[    ]");
+    if(kInitializeKernel64Area() == FALSE){
+        kPrintString( 45, 5, "Fail");
+        kPrintString( 0, 6, "Kernel Area Initialization Fail!!");
+
+        while(1);
+    }
+    kPrintString( 45, 5, "Pass");
 
     while(1);
 }
@@ -16,4 +40,38 @@ void kPrintString( int iX, int iY, const char* pcString ){
     for(i = 0; pcString[i] != 0; i++){
         pstScreen[i].bCharactor = pcString[i];
     }
+}
+
+BOOL kInitializeKernel64Area(void){
+    DWORD* pdwCurrentAddress;
+
+    pdwCurrentAddress = ( DWORD* ) 0x100000;
+
+    while((DWORD)pdwCurrentAddress<0x600000){
+        *pdwCurrentAddress = 0x00;
+
+        if(*pdwCurrentAddress != 0){
+            return FALSE;
+        }
+
+        pdwCurrentAddress++;
+    }
+    return TRUE;
+}
+
+BOOL kIsMemoryEnough(void){
+    DWORD* pdwCurrentAddress;
+
+    pdwCurrentAddress = ( DWORD* ) 0x100000;
+
+    while((DWORD)pdwCurrentAddress<0x400000){
+        *pdwCurrentAddress = 0x12345678;
+
+        if(*pdwCurrentAddress != 0x12345678){
+            return FALSE;
+        }
+
+        pdwCurrentAddress += (0x100000/4);
+    }
+    return TRUE;
 }
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo `qemu-system-x86_64 -L . -fda ./Disk.img -M pc`
+echo `qemu-system-x86_64 -L . -m 32 -fda ./Disk.img -M pc`
