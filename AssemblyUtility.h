1a81abe424eb564e153ed3b35efc961131aaad97
yunhyukNam <DoubleY031223@gmail.com>
2023-03-21 17:36:01 +0900

    Delete AssemblyUtility.h

--- AssemblyUtility.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ASSEMBLYUTILITY_H__
-#define __ASSEMBLYUTILITY_H__
-
-#include "Types.h"
-
-BYTE kInPortByte(WORD wPort);
-void kOutPortByte(WORD wPort, BYTE bData);
-
-#endif /*__ASSEMBLY_UTILITY__*/
